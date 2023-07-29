7618728cd3785991945a7740d796a09c9ddd1313
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-07-29 17:46:20 +0700

    initial commit

---
+++ common/post-fs-data.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+
+
+function setx() {
+  [[ ! -x "${0%/*}/$1" ]] && chmod +x "${0%/*}/$1"
+}
+  setx "system/bin/cpulock"
+# This script will be executed in post-fs-data mode
---
+++ install.sh
@@ -0,0 +1,29 @@
+SKIPMOUNT=false
+PROPFILE=false
+POSTFSDATA=true
+LATESTARTSERVICE=false
+
+REPLACE_EXAMPLE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+REPLACE="
+"
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "     CPULOCK X THERMALKILLER    "
+  ui_print "*******************************"
+}
+
+on_install() {
+  ui_print "- Extracting module files"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+
+set_permissions() {
+  set_perm_recursive $MODPATH 0 0 0755 0644
+}
